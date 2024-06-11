02bfff54360a65037f10233a57808f705f6b621f
BrandonMiyazaki <brandon@miyazaki.cloud>
2024-06-11 15:35:33 -0700

    updates

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+  type        = string
+  description = "name of the app"
+  default     = "tfdemo"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Resources"
+  default     = "westus2"
+}
